f1ca4d72674f0a5bb3188f0cdaf1489e2def631d
Enmanuel Reyes <EnmanuelReyes@live.com>
2018-03-13 00:05:42 -0400

    Finishing Section 9

---
+++ screens/ReviewScreen.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { View, Text, Platform } from 'react-native';
+import { Button } from 'react-native-elements';
+import { withNavigation } from 'react-navigation';
+
+@withNavigation
+class ReviewScreen extends Component {
+  static navigationOptions = ({navigation}) => ({
+      title: 'Review Jobs',
+      headerRight: (
+        <Button
+          title="Settings"
+          onPress={() => navigation.navigate('settings')}
+          backgroundColor="rgba(0,0,0,0)"
+          color="rgba(0, 122, 255, 1)"
+        />
+      ),
+      headerStyle: {
+        marginTop: Platform.Os === 'android' ? 24 : 0
+      }
+  })
+
+  render() {
+    return (
+      <View>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default ReviewScreen;
